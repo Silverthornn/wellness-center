19d909324a97e798a57d99398c7b0b1b783b66ba
silverthornn <joshuajason403@gmail.com>
2024-01-02 21:14:03 +0000

    adding styles

--- screens/signUpScreen.js
+++ screens/signUpScreen.js
@@ -11,4 +11,11 @@ const signUpScreen = () => {
 
 export default signUpScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+})
