0677d7ef09f3f8e8726abc39904f115987f56f33
silverthornn <joshuajason403@gmail.com>
2024-01-09 19:56:05 +0000

    minor update, button

--- screens/doctorSignUp.js
+++ screens/doctorSignUp.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const doctorSignUp = () => {
   return (
     <View>
       <Text>doctorSignUp</Text>
+      <Button title='Next' />
     </View>
   )
 }
